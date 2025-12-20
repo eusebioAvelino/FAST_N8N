5f76a334342073cce21b26543f61865c1ebf58b6
eusebio avelino <eusebioavelino@gmail.com>
2025-12-20 01:23:07 -0400

    python modificacion api

--- FAST_N8N.py
+++ FAST_N8N.py
@@ -1,17 +1,27 @@
-from fastapi import FastAPI, Request
+
 import uvicorn as aplicacion 
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
 
-@app.post("/n8n")
-async def recibir_n8n(request: Request):
-    data = await request.json()
-    print("RECIBIDO:", data)
-    return {
-        "status": "ok",
-        "total": len(data.get("items", []))
-    }
+class Job(BaseModel):
+    title: str
+    company: str
+    link: str
+    date: str
 
+class JobsPayload(BaseModel):
+    items: List[Job]
+
+@app.post("/n8n")
+async def recibir_n8n(payload: JobsPayload):
+    return {
+        "total": len(payload.items),
+        "jobs": payload.items
+    }
 
 if __name__ == "__main__":
     aplicacion.run("FAST_N8N:app", host="0.0.0.0", port=8000, reload=True) 
