f61e1c1986d5688070ffaf9cdbd4d9f80a30bc0b
eusebio avelino <eusebioavelino@gmail.com>
2025-12-27 21:22:48 -0400

    recibe mensaje de n8n-3

--- FAST_N8N.py
+++ FAST_N8N.py
@@ -6,13 +6,30 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+# 🔴 variable global (simple y válida para pruebas)
+ultimo_mensaje = None
+
 class MessageIn(BaseModel):
     output: str
 
+# RECIBIR desde n8n / WhatsApp
 @app.post("/from-n8n")
 async def from_n8n(data: MessageIn):
-    print("Mensaje recibido desde n8n:", data.output)
-    return {"status": "ok", "respuesta": f"Recibido: {data.output}"}
+    global ultimo_mensaje
+    ultimo_mensaje = data.output
+    print("Mensaje recibido:", ultimo_mensaje)
+    return {"status": "ok"}
+
+# LEER desde otra app
+@app.get("/from-n8n")
+async def obtener():
+    return {
+        "ultimo_mensaje": ultimo_mensaje
+    }
+
+if __name__ == "__main__":
+    aplicacion.run("FAST_N8N:app", host="0.0.0.0", port=8000, reload=True)
+
 
 
 
